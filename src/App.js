05bdab60a5a1f1830478b6e477214846295f1e2c
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-11-05 14:22:58 -0800

    Added option to hide editor tabs.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import CodeInput from './CodeInput';
-import ResultFrame from './ResultFrame';
-import defaultCode from './defaultCode.json';
-import parserBabel from 'prettier/parser-babylon';
-import parserPostCSS from 'prettier/parser-postcss';
-import parserHTML from 'prettier/parser-html';
-import prettier from 'prettier/standalone';
+import React, { useState } from "react";
+import styled from "styled-components";
+import CodeInput from "./CodeInput";
+import ResultFrame from "./ResultFrame";
+import defaultCode from "./defaultCode.json";
+import parserBabel from "prettier/parser-babylon";
+import parserPostCSS from "prettier/parser-postcss";
+import parserHTML from "prettier/parser-html";
+import prettier from "prettier/standalone";
 
 export default function App() {
   const [html, setHTML] = useState(defaultCode.html);
   const [css, setCSS] = useState(defaultCode.css);
   const [js, setJS] = useState(defaultCode.js);
-  const [iFrameCode, setiFrameCode] = useState('');
+  const [editorTabs, setEditorTabs] = useState(3);
+  const [iFrameCode, setiFrameCode] = useState("");
+  const [editHTML, setEditHTML] = useState(true);
+  const [editCSS, setEditCSS] = useState(true);
+  const [editJS, setEditJS] = useState(true);
   function updateIframe() {
     const code = `
     <html>
@@ -34,32 +38,101 @@ export default function App() {
     setiFrameCode(code);
   }
   function clearIframe() {
-    setiFrameCode('');
+    setiFrameCode("");
   }
   function formatCode() {
     const newHTML = prettier.format(html, {
-      parser: 'html',
-      plugins: [parserHTML],
+      parser: "html",
+      plugins: [parserHTML]
     });
     const newCSS = prettier.format(css, {
-      parser: 'css',
-      plugins: [parserPostCSS],
+      parser: "css",
+      plugins: [parserPostCSS]
     });
     const newJS = prettier.format(js, {
-      parser: 'babel',
-      plugins: [parserBabel],
+      parser: "babel",
+      plugins: [parserBabel]
     });
     setHTML(newHTML);
     setCSS(newCSS);
     setJS(newJS);
   }
+  function toggleEditor(toggleField) {
+    let html = editHTML;
+    let css = editCSS;
+    let js = editJS;
+    switch (toggleField) {
+      case "html":
+        html = !html;
+        setEditHTML(html);
+        break;
+      case "css":
+        css = !css;
+        setEditCSS(css);
+        break;
+      case "js":
+      default:
+        js = !js;
+        setEditJS(js);
+        break;
+    }
+    const newEditorTabs = html + css + js;
+    setEditorTabs(newEditorTabs);
+  }
   return (
     <AppDiv>
       <PageHeader>Web Snippet Tester</PageHeader>
+      <ViewTabs>
+        <ViewTabLabel>
+          HTML:{" "}
+          <input
+            type="checkbox"
+            checked={editHTML}
+            onChange={() => toggleEditor("html")}
+          />
+        </ViewTabLabel>
+        <ViewTabLabel>
+          CSS:{" "}
+          <input
+            type="checkbox"
+            checked={editCSS}
+            onChange={() => toggleEditor("css")}
+          />
+        </ViewTabLabel>
+        <ViewTabLabel>
+          JS:{" "}
+          <input
+            type="checkbox"
+            checked={editJS}
+            onChange={() => toggleEditor("js")}
+          />
+        </ViewTabLabel>
+      </ViewTabs>
       <CodingArea>
-        <CodeInput codeType='HTML' code={html} setCode={setHTML} />
-        <CodeInput codeType='CSS' code={css} setCode={setCSS} />
-        <CodeInput codeType='JavaScript' code={js} setCode={setJS} />
+        {editHTML && (
+          <CodeInput
+            codeType="HTML"
+            code={html}
+            setCode={setHTML}
+            editorTabs={editorTabs}
+          />
+        )}
+        {editCSS && (
+          <CodeInput
+            codeType="CSS"
+            code={css}
+            setCode={setCSS}
+            editorTabs={editorTabs}
+          />
+        )}
+        {editJS && (
+          <CodeInput
+            codeType="JavaScript"
+            code={js}
+            setCode={setJS}
+            editorTabs={editorTabs}
+          />
+        )}
       </CodingArea>
       <ControlArea>
         <ControlButton onClick={formatCode}>Format Code</ControlButton>
@@ -73,7 +146,7 @@ export default function App() {
 
 const CodingArea = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   margin: 0 1rem;
 `;
 
@@ -97,4 +170,12 @@ const ControlButton = styled.button`
   border-radius: 3px;
   margin: 0 1rem;
   padding: 0.2rem 0.6rem;
+`;
+
+const ViewTabs = styled.div`
+  text-align: center;
+`;
+
+const ViewTabLabel = styled.label`
+  margin: 0 1rem;
 `;
--- src/CodeInput.js
+++ src/CodeInput.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-export default function CodeInput({ codeType, code, setCode }) {
+export default function CodeInput({ codeType, code, setCode, editorTabs }) {
+  function editorWidth() {
+    switch (editorTabs) {
+      case 1:
+        return "90vw";
+      case 2:
+        return "45vw";
+      case 3:
+      default:
+        return "30vw";
+    }
+  }
   return (
     <div>
       <h2>{codeType}</h2>
-      <TextArea spellCheck={false} value={code} onChange={e => setCode(e.target.value)}></TextArea>
+      <TextArea
+        spellCheck={false}
+        value={code}
+        onChange={e => setCode(e.target.value)}
+        style={{ width: editorWidth(), margin: `0 10px` }}
+      ></TextArea>
     </div>
   );
 }
@@ -15,7 +31,6 @@ const TextArea = styled.textarea`
   color: #eee;
   border: 1px solid #eee;
   height: 40vh;
-  width: 30vw;
   border-radius: 3px;
   padding: 3px;
 `;
